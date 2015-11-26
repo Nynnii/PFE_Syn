62e6f12dfcf74c04487773b24cf95028aab64db0
Etienne <titienne974@hotmail.com>
2015-11-26 15:46:41 +0100

    Merge footer (conflits)

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,14 +2,10 @@
 			<div class="footer-mentions">
 				<div class="container">
 					<div class="row">
-						<p class="col-xs-6 col-sm-3 col-md-3">SYN@2015</p>
-						<a class="col-xs-6 col-sm-3 col-md-3" href="#">Mentions légales</a>
-						<a class="col-xs-6 col-sm-3 col-md-3" href="http://www.groupe-efrei.fr/" target="_blank">Efrei<a/>
-						<a class="col-xs-6 col-sm-3 col-md-3" href="https://www.facebook.com/groups/1511900572456300/" target="_blank"><img id="facebook" src="images/fb_icon.png" alt="Logo facebook"/></a>
 						<a class="col-xs-6 col-sm-3 col-md-3" href="">SYN@2015</a>
 						<a class="col-xs-6 col-sm-3 col-md-3" href="">Mentions légales</a>
 						<a class="col-xs-6 col-sm-3 col-md-3" href="">efrei</a>
-						<a class="col-xs-6 col-sm-3 col-md-3" href="https://www.facebook.com/groups/1511900572456300/"><img id="facebook" src="/syn/images/fb_icon.png" style="height:33px" alt="Logo facebook" /></a>
+						<a class="col-xs-6 col-sm-3 col-md-3" href="https://www.facebook.com/groups/1511900572456300/"><img id="facebook" src="images/fb_icon.png" style="height:33px" alt="Logo facebook" /></a>
 					</div>
 				</div>
 			</div>
